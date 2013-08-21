522d7912bec70be1b2a447ed1d7057fad2590fc0
Andre Arko <andre@arko.net>
2013-08-20 17:20:11 -0700

    Reverse the spec since ipsum should contain text.
    We want to test to make sure that Lorem.ipsum is actually returning some
    lorem text that we can use from other code. The test used to be doing
    the opposite, which means the ipsum method wasn't very helpful.

--- spec/lorem_spec.rb
+++ spec/lorem_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Lorem
   describe Lorem do
     it "should not be empty" do
-     expect(Lorem.ipsum).to be_false
+      expect(Lorem.ipsum).to_not be_empty
     end
   end
 end
