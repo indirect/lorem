28f66578508874878337141f459b03163be7fc06
Andre Arko <andre@arko.net>
2013-08-20 17:20:59 -0700

    Add a spec for the contents of the lorem text.
    Now that the Lorem.ipsum method is returning a string containing
    the text, we test to make sure that the first word of that text
    is actually there, implying that we are getting the right text.

--- spec/lorem_spec.rb
+++ spec/lorem_spec.rb
@@ -5,5 +5,9 @@ module Lorem
     it "should not be empty" do
       expect(Lorem.ipsum).to_not be_empty
     end
+
+    it "should include 'Fantastic'" do
+      expect(Lorem.ipsum).to include('Fantastic')
+    end
   end
 end
